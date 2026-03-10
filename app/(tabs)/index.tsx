fc18deab2504b608c868ac5d57bb02c634d7cf8e
Marina Teixeira <108299821+Marina-teixeira@users.noreply.github.com>
2026-03-10 16:35:16 -0300

    Criando a homescreen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,39 +1,31 @@
-import { api } from "@/convex/_generated/api";
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import Header from "@/components/Header";
 import { useTheme } from "@/hooks/useTheme";
-import { useMutation, useQuery } from "convex/react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
-  
-  const todos = useQuery(api.todos.getTodos);
-  console.log(todos);
+  const { toggleDarkMode, colors } = useTheme();
 
-  const addTodo = useMutation(api.todos.addTodo);
-  
+  const homeStyles = createHomeStyles(colors);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.content}>Edit app/index.tsx to edit this screen.1234</Text>
-      <Text>hi</Text>
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>Toggle the mode</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => addTodo({ text: "walk the dog" })}>
-        <Text>Add a new todo</Text>
-      </TouchableOpacity>
-    </View>
+    <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
+      <StatusBar
+        style={colors.statusBarStyle === "light-content" ? "light" : "dark"}
+        backgroundColor={colors.bg}
+        translucent={false}
+      />
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Header />
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>Toggle the mode</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 10,
-  },
-  content: {
-    fontSize: 22,
-  },
-});
 
