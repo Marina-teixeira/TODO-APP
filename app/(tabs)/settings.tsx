b0098a57c3e129ed559d9018da561707e8376bad
Marina Teixeira <108299821+Marina-teixeira@users.noreply.github.com>
2026-03-24 21:53:53 -0300

    Criando as cofigurações

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,43 @@
-import { Text, View } from "react-native";
+import { createSettingsStyles } from "@/assets/styles/settings.styles";
+import { useTheme } from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import ProgressStats from "@/components/ProgressStats";
+import Preferences from "@/components/Preferences";
+import DangerZone from "@/components/DangerZone";
+
 const SettingsScreen = () => {
+
+    const { colors } = useTheme();
+
+    const settingsStyles = createSettingsStyles(colors);
+
     return (
-        <View>
-            <Text>SettingScreen</Text>
-        </View>
+        <LinearGradient colors={colors.gradients.background} style={settingsStyles.container}>
+            <SafeAreaView style={settingsStyles.safeArea}>
+                {/* HEADER */}
+                <View style={settingsStyles.header}>
+                    <View style={settingsStyles.titleContainer}>
+                        <LinearGradient colors={colors.gradients.primary} style={settingsStyles.iconContainer}>
+                            <Ionicons name="settings" size={28} color="#ffffff" />
+                        </LinearGradient>
+                        <Text style={settingsStyles.title}>Settings</Text>
+                    </View>
+                </View>
+
+                <ScrollView
+                    style={settingsStyles.scrollView}
+                    contentContainerStyle={settingsStyles.content}
+                    showsVerticalScrollIndicator={false}
+                >
+                    <ProgressStats />
+                    <Preferences />
+                    <DangerZone />
+                </ScrollView>
+            </SafeAreaView>
+        </LinearGradient>
     );
 };
 export default SettingsScreen;
